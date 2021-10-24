4135902f57570d4ab68c7eeb138d04daf7b95d15
ikrasnova <i.bas@me.com>
2021-10-24 21:05:45 +0300

    with updates from 24.10

--- src/telran/util/List.java
+++ src/telran/util/List.java
@@ -1,5 +1,7 @@
 package telran.util;
 
+import java.util.function.Predicate;
+
 public interface List<T> {
 /**
  * adds a given element at end of list
@@ -31,5 +33,52 @@ public interface List<T> {
  * @return reference to being removed element or null if the index is incorrect
  */
 	T remove (int index );
+	/**
+	 * check existence of an object
+	 * @param pattern
+	 * @return true if there is at least one object equaled to a given pattern^ otherwise - false
+	 */
+	boolean contains(T pattern);
+	/**
+	 * 
+	 * @param pattern
+	 * @return index of the first occurrence of an object equaled to a given pattern, or -1 of no such an object
+	 * 
+	 */
+	int indexOf(T pattern);
+	/**
+	 * 
+	 * @param pattern
+	 * @return index of the last occurrence of an object equaled to a given pattern, or -1 of no such an object
+	 * 
+	 */
+	int lastIndexOf(T pattern);
+	/**
+	 * 
+	 * @param predicate
+	 * @return true in the case the list contains ate list one object matching a condition of a given predicate
+	 */
+	boolean contains(Predicate<T> predicate);
+	/**
+	 * 
+	 * @param pattern
+	 * @return index of the first occurrence of an object equaled to a given predicate, or -1 of no such an object
+	 * 
+	 */
+	int indexOf(Predicate<T> predicate);
+	/**
+	 * 
+	 * @param pattern
+	 * @return index of the last occurrence of an object equaled to a given predicate, or -1 of no such an object
+	 * 
+	 */
+	int lastIndexOf(Predicate<T> predicate);
+	/**
+	 * removes all objects matching a given predicate if at list one  object has been removed otherwise -false
+	 * @param predicate
+	 * @return
+	 */
+	 
+	boolean removeIf(Predicate<T> predicate);
 	
 }
